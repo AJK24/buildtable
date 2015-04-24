72d50fe59fd6bb5882019e187d2d38dc6bc753b4
Andrew <akv9509@saintjoe.edu>
2015-04-23 23:17:36 -0400

    Updated tablebuild.js

--- tablebuild.js
+++ tablebuild.js
@@ -2,10 +2,12 @@
 
 function tablebuild(data) {
 
+	//Create table elements
 	var table = document.createElement("table");
 	var fields = Object.keys(data[0]);
 	var headRow = document.createElement("tr");
 
+	//Checks for the "th" in the fields
 	fields.forEach(function(field) {
 		var headCell = document.createElement("th");
 		headCell.textContent = field;
@@ -13,4 +15,27 @@ function tablebuild(data) {
 
 	});
 
-}
+	//adds the new rows
+	table.appendChild(headRow);
+	data.forEach(function(object) {
+
+		var row = document.createElement("tr");
+		fields.forEach(function(field) {
+
+			var cell = document.createElement("td");
+			cell.textContent = object[field];
+			if (typeof object[field] == "number")
+				cell.style.textAlign = "right";
+
+		row.appendChild(cell);
+		});
+
+		table.appendChild(row);
+	
+	});
+	return table;
+}}
+
+  document.body.appendChild(buildTable(MOUNTAINS));
+
+
